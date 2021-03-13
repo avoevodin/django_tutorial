6b6b6509db1e3af0854d96a4039f7a4acb7cc06e
Andrey Voevodin <avoevodin8888@gmail.com>
2021-03-13 21:08:13 +0800

    tuned detail view with render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse, Http404
 from .models import Question
+from django.shortcuts import render, get_object_or_404
 
 
 def index(request):
@@ -15,10 +12,7 @@ def index(request):
 
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk = question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question doesn't exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {"question": question})
 
 
