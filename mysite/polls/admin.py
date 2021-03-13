fc6819abdb84e187c46cbf104b357f045bead099
Andrey Voevodin <avoevodin8888@gmail.com>
2021-03-13 17:56:44 +0800

    added admin models tuning

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-# Register your models here.
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = [
+        'id', 'question_text', 'pub_date',
+    ]
+    readonly_fields = [
+        'id'
+    ]
+    list_display = [
+        'id', 'question_text', 'pub_date', 'was_published_recently',
+    ]
+    list_display_links = [
+        'id', 'question_text'
+    ]
+    search_fields = [
+        '^question_text'
+    ]
+    list_filter = [
+        'pub_date'
+    ]
+
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
