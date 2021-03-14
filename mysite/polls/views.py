11df924e60b739a8de01e16a32a6191c2639b14b
Andrey Voevodin <avoevodin8888@gmail.com>
2021-03-14 15:12:36 +0800

    added test of index view and corrected typo in index.html. Fixed bug in IndexView: only questions with pub_date in past will be displayed on index.html.

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.db.models import F
 from django.views import generic
+from django.utils import timezone
 
 
 class IndexView(generic.ListView):
@@ -11,8 +12,13 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set
+        to be published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
